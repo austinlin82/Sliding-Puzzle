044034966e51ebff94b2f42668f07b5b71fa79aa
Austin Lin <austin.lin.82@gmail.com>
2021-04-28 20:41:26 -0400

    commit

--- sliding-puzzle/src/App.js
+++ sliding-puzzle/src/App.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text } from 'react-konva';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      
-    </div>
+    <Stage width={window.innerWidth} height = {window.innerHeight}>
+      <Layer>
+        <Rect
+          x={20}
+          y = {50}
+          width = {100}
+          height = {100}
+          fill="blue"
+        />
+        <Text
+          x="70"
+          y= "120"
+          fill="white">
+            Hello
+        </Text>
+      </Layer>
+    </Stage>
   );
 }
 
